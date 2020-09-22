9d6fae2de2ed4190d53e67ca7e8287d1bf00f8e2
Vinny V <VinAVarghese@gmail.com>
2020-09-21 18:11:48 -0700

    added display none func

--- client/src/components/Loading/loading.js
+++ client/src/components/Loading/loading.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import './loading.css'
 
 export default function Loading(props) {
+
+    const [className, setClassName] = useState("loading-div")
+
+    const displayNone = () => {
+        if (props.display === true) {
+            console.log("waiting");
+        }else {
+            let timeleft = 1
+            let countdown = setInterval(() => {
+                if (timeleft === 0){
+                    setClassName("disappear");
+                    clearInterval(countdown);
+                    console.log("complete")
+                } else {
+                    timeleft = timeleft - 1;
+                    console.log("running");
+                }
+            }, 250);
+        }
+    }
+
+    useEffect(() => {
+        displayNone()
+    }, [props.display])
+
     return (
-        <div className="loading-div" style={{ opacity: props.display ? 1 : 0}}>
+        <div className={className} style={{ opacity: props.display ? 1 : 0}}>
             <img className="loading-logo" src="./images/logothumbnail.png" alt="logo"/>
         </div>
     )
