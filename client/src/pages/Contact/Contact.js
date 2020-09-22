5990d1efa30ddf3616406d033ad3a17455d04058
Vinny V <VinAVarghese@gmail.com>
2020-09-22 13:46:45 -0700

    adjusting watermark and contact page misspacing

--- client/src/pages/Contact/Contact.js
+++ client/src/pages/Contact/Contact.js
@@ -28,20 +28,18 @@ export default function Contact() {
         <>
         <Loading display={loading}/>
         <Hero text="Contact"/>
-        <section className="row justify-content-center">
-            <section className="col-9 thankyou">
-                <h2>Thank You</h2>
-                <p>Thank you for visiting my site today. <br/> I'd love for you to get in touch, feel free to send me an email. <br/> You can also find links to my social pages, please connect! <br/> Have a great day and here's to creating something meaningful!</p>
-                <a className="email-link" href = "mailto:VinAVarghese@gmail.com"><strong>VinAVarghese@gmail.com</strong></a>
-                <br/>
-                <br/>
-                <a href="https://www.linkedin.com/in/vincent-varghese-49009b1a8/">
-                    <img className="sm-logos" src="./images/linkedinlogo.png" alt="linkedinlogo" target="_blank"/>
-                </a>
-                <a href="https://github.com/VinAVarghese">
-                    <img className="sm-logos" src="./images/github.png" alt="githublogo" target="_blank"/>
-                </a>
-            </section>
+        <section className="thankyou">
+            <h2>Thank You</h2>
+            <p>Thank you for visiting my site today. <br/> I'd love for you to get in touch, feel free to send me an email. <br/> You can also find links to my social pages, please connect! <br/> Have a great day and here's to creating something meaningful!</p>
+            <a className="email-link" href = "mailto:VinAVarghese@gmail.com"><strong>VinAVarghese@gmail.com</strong></a>
+            <br/>
+            <br/>
+            <a href="https://www.linkedin.com/in/vincent-varghese-49009b1a8/">
+                <img className="sm-logos" src="./images/linkedinlogo.png" alt="linkedinlogo" target="_blank"/>
+            </a>
+            <a href="https://github.com/VinAVarghese">
+                <img className="sm-logos" src="./images/github.png" alt="githublogo" target="_blank"/>
+            </a>
         </section>
         {/* <EmailForm /> */}
         <Watermark />
