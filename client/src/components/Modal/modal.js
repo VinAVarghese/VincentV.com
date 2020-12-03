db94df12fea2db0e4f3a06b3cc5f8c43676482dd
Vinny V <VinAVarghese@gmail.com>
2020-12-03 14:23:04 -0800

    adjusting bootstrap code for modal and adjusting loading positioning for smaller devices

--- client/src/components/Modal/modal.js
+++ client/src/components/Modal/modal.js
@@ -14,13 +14,12 @@ export default function Modal(props) {
     }    
 
     return (
-        <div className="on">
-        {/* {modalVisible?"on":"off"}> */}
+        <div className={modalVisible?"on":"off"}>
             <button className="click-off" onClick={toggleModal}>
                 <div className="modal-div">
                     <div className="x">X</div>
-                    <div className="container align-items-center content-div-height">
-                        <div className="row justify-content-center">
+                    <div className="container">
+                        <div className="content-div row justify-content-center">
                             <h1>The ArrowPeaks</h1>
                             <img className="m-logo" src="./images/logothumbnail.png" alt="logo-r"/>  
                             <h4>A logo I created for myself and my portfolio. The image is reminiscent of mountain peaks both upright and upended or, arrows pointing here and to. <br/> <br/> I love the idea of working together to reach new heights, collaborating to challenge the norm, and developing to bring upon change. <br/><br/> The ArrowPeaks represent team work that subverts what was thought possible and looks at challenges as opportunities.</h4> 
